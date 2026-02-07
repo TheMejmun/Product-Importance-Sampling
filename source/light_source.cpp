0942c79c41d1769f870b8f9379044f0a85329523
Saman Miran <c@samanmiran.com>
2026-02-07 13:14:08 +0100

    Add more light sampling algorithms

--- source/light_source.cpp
+++ source/light_source.cpp
@@ -13,8 +13,7 @@ namespace {
     std::uniform_int_distribution<uint32_t> randIntDistr(0, std::numeric_limits<uint32_t>::max());
 }
 
-
-float sampling::sample_lights(const std::vector<LightSource> &lightSources, const Polar &wo) {
+float sample_lights_random(const std::vector<LightSource> &lightSources, const Polar &wo) {
     std::vector<LightSource const *> reachableLights;
     for (const auto &light: lightSources) {
         if (light.start_angle <= wo.phi && light.end_angle >= wo.phi) {
@@ -26,4 +25,30 @@ float sampling::sample_lights(const std::vector<LightSource> &lightSources, cons
                           ? 0
                           : reachableLights[randIntDistr(randEng) % reachableLights.size()]->intensity;
     return intensity;
+}
+
+float sample_lights_additive(const std::vector<LightSource> &lightSources, const Polar &wo) {
+    float intensity = 0.f;
+    for (const auto &light: lightSources) {
+        if (light.start_angle <= wo.phi && light.end_angle >= wo.phi) {
+            intensity += light.intensity;
+        }
+    }
+    return intensity;
+}
+
+float sample_lights_average(const std::vector<LightSource> &lightSources, const Polar &wo) {
+    float intensity = 0.f;
+    size_t count = 0;
+    for (const auto &light: lightSources) {
+        if (light.start_angle <= wo.phi && light.end_angle >= wo.phi) {
+            intensity += light.intensity;
+            ++count;
+        }
+    }
+    return intensity / static_cast<float>(count);
+}
+
+float sampling::sample_lights(const std::vector<LightSource> &lightSources, const Polar &wo) {
+    return sample_lights_random(lightSources, wo);
 }
